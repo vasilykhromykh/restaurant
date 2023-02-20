e8d10918353f3a945114296fb2be9cc5d6ba746f
vasilykhromykh <vasilykhromykh@gmail.com>
2023-02-20 12:29:25 +0300

    1 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+import TitlePage from "./Components/TitlePage/TitlePage";
+import MainPage from './Components/MainPage/MainPage';
+import MapPage from "./Components/MapPage/MapPage";
+import {Route, Routes} from 'react-router-dom';
+import ReservePage from "./Components/ReservePage/ReservePage";
+import GalleryPage from "./Components/GalleryPage/GalleryPage";
+import MenuPage from "./Components/MenuPage/MenuPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    function Scroll() {
+
+        window.scrollTo({
+            top: 9000,
+            behavior: "smooth"
+        });
+    }
+console.log(window.location.pathname)
+
+    return (
+
+        <div className="App">
+
+
+
+            <Routes>
+                <Route path="" element={
+                    <>
+                        <button onClick={Scroll} className="hover-underline-animation">Заказать Столик</button>
+                   <TitlePage/>
+                    <MainPage/>
+                    </>
+                }>
+
+
+                </Route>
+            </Routes>
+            <Routes>
+                <Route path="/map" element={
+                   <MapPage/>
+                }>
+
+                </Route>
+            </Routes>
+            <Routes>
+                <Route path="/reserve" element={
+                    <ReservePage/>
+                }>
+
+                </Route>
+            </Routes>
+            <Routes>
+                <Route path="/gallery" element={
+                    <GalleryPage/>
+                }>
+
+                </Route>
+            </Routes>
+            <Routes>
+                <Route path="/menu" element={
+                    <MenuPage/>
+                }>
+
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
